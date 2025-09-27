4b717ee59bd20fe26dc816fb3e169e3500fec904
indra <indra1nkuss@gmail.com>
2025-09-27 22:36:45 +0700

    Menambahkan operasi kali dan bagi

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -2,6 +2,10 @@ void main() {
   final a = _readNumber("Masukkan angka pertama");
   final b = _readNumber("Masukkan angka kedua");
 
-  print("Hasil tambah: ${a + b}");
-  print("Hasil kurang: ${a - b}");
+  print("Hasil kali: ${a * b}");
+  if (b != 0) {
+    print("Hasil bagi: ${a / b}");
+  } else {
+    print("Tidak bisa membagi dengan nol");
+  }
 }
