cb1a8b87f33be1b528c2eadae5f3aaa9c100f924
indra <indra1nkuss@gmail.com>
2025-09-27 21:59:52 +0700

    tugas week 2 dart

---
+++ bin/tugas_week_2_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week_2_dart/tugas_week_2_dart.dart' as tugas_week_2_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week_2_dart.calculate()}!');
+}
---
+++ test/tugas_week_2_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week_2_dart/tugas_week_2_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
